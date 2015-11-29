5734b600a89d3b606e2cad7aa3a34014ff19172d
Yahaire <yahaire5@hotmail.com>
2015-11-29 17:26:24 -0600

    Se agregó el código para tabla de paginación
    El código fue escrito por Adrián. Se tuvo que subir en mi cuenta por
    errores de GitHub en la máquina de Adrián.

--- Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/TablaDePaginacion.java
+++ Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/TablaDePaginacion.java
@@ -1,5 +1,59 @@
 package trabajoFinal.cps;
 
-public class TablaDePaginacion {
+import java.util.Iterator;
+import java.util.Vector;
 
+public class TablaDePaginacion {
+    //Guarda la dirección en memoria de la página [n] del vector.
+    Vector <Pagina> vecPaginas;
+    // ID del proceso al cual pertenece la tabla de paginación.
+    int iIDProceso;
+
+    /**
+     * Inicializa iIDProceso, y el vector de Páginas.
+     *
+     * @param iIDProceso es el ID del prcoceso a inicializar.
+     * @param iNumPaginas es la cantidad de paginas del proceso.
+     */
+    public void TablaDePaginacion(int iIDProceso, int iNumPaginas) {
+        this.iIDProceso = iIDProceso;
+        vecPaginas.setSize(iNumPaginas);
+    }
+
+    /**
+     * Método utilizado para obtener las direcciones de memoria que se deben
+     * liberar
+     *
+     * @return Un vector de enteros que representa las direcciones
+     * en memoria a liberar.
+     */
+    public Vector<Integer> liberarMemoria() {
+
+        Vector<Integer> vecLiberar = new Vector<>();
+
+        for (Pagina p : vecPaginas) {
+            vecLiberar.addElement(p.getDirFisica);
+        }
+
+        return vecLiberar;
+    }
+
+    /**
+     * Método utilizado para obtener las direcciones de swap que se deben
+     * liberar.
+     *
+     * @return Un vector de enteros que representa las direcciones
+     * en Swap a liberar.
+     */
+    public Vector<Integer> liberarSwap() {
+
+        Vector<Integer> vecLiberar = new Vector<>();
+
+        for (Pagina p : vecPaginas) {
+            vecLiberar.addElement(p.iDirSwap);
+        }
+
+        return vecLiberar;
+    }
 }
+
