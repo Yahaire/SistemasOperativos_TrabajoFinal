99564aaa39cb824ec9a668f62054fd0b5f17f8f7
Ktl_XV <jorge@ktlxv.com>
2015-11-30 13:19:22 -0600

    Cambiar el encoding de Archivo a UTF-8

--- Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/Archivo.java
+++ Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/Archivo.java
@@ -10,29 +10,29 @@ import java.util.StringTokenizer;
 
 public class Archivo {
 	private String sNombre;			// nombre del archivo
-	public Vector<Double> vecX;		//%m// PosiciÛn en X de los puntos
-	public Vector<Double> vecY;		//%m// PosiciÛn en Y de los puntos
+	public Vector<Double> vecX;		//%m// Posici√≥n en X de los puntos
+	public Vector<Double> vecY;		//%m// Posici√≥n en Y de los puntos
 	public Vector<Instruccion> vecInstrucciones;
-	public double dXk;				//%m// Guarda el dato Xk del archivo (primer dato a leer)				
-	
+	public double dXk;				//%m// Guarda el dato Xk del archivo (primer dato a leer)
+
 	/*
 	 * Archivo
-	 * 
+	 *
 	 * Constructor default
 	 */
 	public Archivo() {
-		sNombre = "";	
+		sNombre = "";
 		vecX = new Vector<Double>();	//%m
 		vecY = new Vector<Double>();	//%m
 		vecInstrucciones = new Vector<Instruccion>();
 		dXk = -1;					//%m
 	}
-																	
+
 	/*
 	 * Archivo (2)
-	 * 
+	 *
 	 * Constructor con nombre
-	 * 
+	 *
 	 * @param sNombre es el nombre <code>String</code> del archivo
 	 */
 	public Archivo( String sNombre ) {
@@ -45,9 +45,9 @@ public class Archivo {
 
 	/*
 	 * setNombre
-	 * 
+	 *
 	 * Set de sNombre
-	 * 
+	 *
 	 * @param sNombre es el nombre <code>String</code> del archivo
 	 */
 	public void setNombre( String sNombre ) {
@@ -56,41 +56,41 @@ public class Archivo {
 
 	/*
 	 * getNombre
-	 * 
+	 *
 	 * Get de sNombre
-	 * 
+	 *
 	 * @return el nombre <code>String</code> del archivo
 	 */
 	public String getNombre() {
 		return this.sNombre;
 	}
-	
+
 	/*
 	 * Agregar Instruccion
-	 * 
-	 * AÒade una instrucciÛn nueva.
-	 * Regresa un string vacÌo ("") si se tuvo Èxito, y el mensaje de error, si no.
-	 * 
-	 * @param sInstruccion es el <code>String</code> de la instrucciÛn a agregar
-	 * @param iLinea es el n˙mero <code>int</code> de lÌnea que se est· intentando agregar (para mensajes de error)
-	 * 
+	 *
+	 * A√±ade una instrucci√≥n nueva.
+	 * Regresa un string vac√≠o ("") si se tuvo √©xito, y el mensaje de error, si no.
+	 *
+	 * @param sInstruccion es el <code>String</code> de la instrucci√≥n a agregar
+	 * @param iLinea es el n√∫mero <code>int</code> de l√≠nea que se est√° intentando agregar (para mensajes de error)
+	 *
 	 * @return el mensaje <code>String</code> del error encontrado, si es que hay
 	 */
-	public String AgregarInstruccion( String sInstruccion, int iLinea ) {	//%m		
-		String sMensajeError = "No se ha podido agregar la lÌnea " + iLinea + ". ";
+	public String AgregarInstruccion( String sInstruccion, int iLinea ) {	//%m
+		String sMensajeError = "No se ha podido agregar la l√≠nea " + iLinea + ". ";
 		boolean bError = false;
-		
-		if ( !sInstruccion.trim().equals("") ) {	// Revisar que la instrucciÛn no sea una lÌnea vacÌa
+
+		if ( !sInstruccion.trim().equals("") ) {	// Revisar que la instrucci√≥n no sea una l√≠nea vac√≠a
 			StringTokenizer tokEntrada = new StringTokenizer(sInstruccion, " ");
-			
+
 			String sDefinidor = tokEntrada.nextToken().trim();
-			
+
 			int[] iValores = new int[3];	// Valores a usar para guardar como objeto Instruccion
-			
+
 			iValores[0] = -1;
 			iValores[1] = -1;
 			iValores[2] = -1;
-			
+
 			try {
 				for (int iK = 0; iK < tokEntrada.countTokens() - 1; iK++) {
 					iValores[iK] = Integer.parseInt( tokEntrada.nextToken().trim() );
@@ -99,16 +99,16 @@ public class Archivo {
 				bError = true;
 				sMensajeError = sMensajeError + "Alguno de los datos de la instruccion se encuentra mal escrito.\n";
 			}
-			
+
 			if ( !bError ) {
 				Instruccion insAAgregar = new Instruccion(sDefinidor, iValores[0], iValores[1], iValores[2]);
-				
+
 				if ( insAAgregar.esInstruccionValida() ) {
 					this.vecInstrucciones.addElement(insAAgregar);
 				}
 				else {
 					bError = true;
-					sMensajeError = sMensajeError + "El primer caracter de la instrucciÛn no es correcto.\n";
+					sMensajeError = sMensajeError + "El primer caracter de la instrucci√≥n no es correcto.\n";
 				}
 			}
 		}
@@ -116,7 +116,7 @@ public class Archivo {
 			bError = true;
 			sMensajeError = sMensajeError + "La linea parece estar vacia\n";
 		}
-		
+
 		if ( bError ) {
 			return sMensajeError;
 		}
@@ -124,35 +124,35 @@ public class Archivo {
 			return "";
 		}
 	}
-	
+
 	class Instruccion {
-		protected String sTipo;	// Tipo de instrucciÛn
-		int iValor1;	// Valor 1 (en caso de necesitarse para la instrucciÛn)
-		int iValor2;	// Valor 2 (en caso de necesitarse para la instrucciÛn)
-		int iValor3;	// Valor 3 (en caso de necesitarse para la instrucciÛn)
-		int iCantValDisp;	// Cantidad de valores disponibles. Depende del tipo de instrucciÛn.
-		boolean bInstruccionValida;	// Indica si la instrucciÛn es v·lida o no
-		
+		protected String sTipo;	// Tipo de instrucci√≥n
+		int iValor1;	// Valor 1 (en caso de necesitarse para la instrucci√≥n)
+		int iValor2;	// Valor 2 (en caso de necesitarse para la instrucci√≥n)
+		int iValor3;	// Valor 3 (en caso de necesitarse para la instrucci√≥n)
+		int iCantValDisp;	// Cantidad de valores disponibles. Depende del tipo de instrucci√≥n.
+		boolean bInstruccionValida;	// Indica si la instrucci√≥n es v√°lida o no
+
 		/*
 		 * Instruccion
-		 * 
-		 * Constructor parametrizado de la subclase. Se necesitan valor para todos los par·metros. En caso 
-		 * de no necesitarse, se ignorar· el par·metro (depende del tipo).
-		 * 
-		 * @sTipo es el tipo en <code>String</code> de la instrucciÛn. Se usar· para saber cÛmo manejar los otros valores
+		 *
+		 * Constructor parametrizado de la subclase. Se necesitan valor para todos los par√°metros. En caso
+		 * de no necesitarse, se ignorar√° el par√°metro (depende del tipo).
+		 *
+		 * @sTipo es el tipo en <code>String</code> de la instrucci√≥n. Se usar√° para saber c√≥mo manejar los otros valores
 		 * @iVal1 es el valor <code>int</code> del primer dato (en caso de ser necesario)
 		 * @iVal2 es el valor <code>int</code> del segundo dato (en caso de ser necesario)
 		 * @iVal3 es el valor <code>int</code> del tercer dato (en caso de ser necesario)
 		 */
 		public Instruccion( String sTipo, int iVal1, int iVal2, int iVal3 ) {
-			
+
 			this.bInstruccionValida = true;
-			
+
 			this.iValor1 = iVal1;
 			this.iValor2 = iVal2;
 			this.iValor3 = iVal3;
-			
-			// Verificar que la isntrucciÛn sea v·lida y designar cu·ntos valores se pueden usar
+
+			// Verificar que la instrucci√≥n sea v√°lida y designar cu√°ntos valores se pueden usar
 			if (sTipo.equals("P")) {
 				this.iCantValDisp = 2;
 			}
@@ -168,11 +168,11 @@ public class Archivo {
 			else if (sTipo.equals("E")) {
 				this.iCantValDisp = 0;
 			}
-			else {	// InstrucciÛn no v·lida
+			else {	// Instrucci√≥n no v√°lida
 				bInstruccionValida = false;
 				this.iCantValDisp = -1;
 			}
-			
+
 			// Asignar tipo
 			if ( bInstruccionValida ) {
 				this.sTipo = sTipo;
@@ -181,16 +181,16 @@ public class Archivo {
 				this.sTipo = "Instruccion Invalida";
 			}
 		}
-		
+
 		/*
-		 * Es InstrucciÛn V·lida
-		 * 
-		 * Regresa si la instrucciÛn es v·lida
+		 * Es Instrucci√≥n V√°lida
+		 *
+		 * Regresa si la instrucci√≥n es v√°lida
 		 */
 		public boolean esInstruccionValida() {
 			return bInstruccionValida;
 		}
-		
+
 		public int getValor1() {
 			if (iCantValDisp > 0) {
 				return this.iValor1;
@@ -199,7 +199,7 @@ public class Archivo {
 				return -1;
 			}
 		}
-		
+
 		public int getValor2() {
 			if (iCantValDisp > 1) {
 				return this.iValor2;
@@ -208,7 +208,7 @@ public class Archivo {
 				return -1;
 			}
 		}
-		
+
 		public int getValor3() {
 			if (iCantValDisp > 2) {
 				return this.iValor3;
@@ -218,6 +218,5 @@ public class Archivo {
 			}
 		}
 	}
-		
-}
 
+}
