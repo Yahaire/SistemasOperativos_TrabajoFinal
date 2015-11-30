bb15a0b7b74d9a2dcf1d0f7cbfedf8b4d8009f95
Yahaire <yahaire5@hotmail.com>
2015-11-29 23:32:04 -0600

    Modifiqué la clase Archivo y la clase Main
    La clase archivo sirve para almacenar las instrucciones leídas del
    archivo provisto. Funciona para solo almacenar aquellas instrucciones
    correctas. La clase está terminada.
    
    El Main está casi listo. Falta limpiar un poco el código (quitar
    mensajes de errores no apropiados y comentarios viejos), asegurarse que
    se muestran las salidas esperadas, y manejar apropiadamente los datos
    recibidos del método liberar de ManejadorDeMemoria.

--- Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/Main.java
+++ Sistemas Operativos Trabajo Final/src/trabajoFinal/cps/Main.java
@@ -40,42 +40,80 @@ public class Main {
 					// Leer y guardar información del archivo
 					arcEntrada.setNombre(sRespuesta);
 																												////14
-					double dAux;	//%m
-					if ((sRespuesta = bfrArchivoEntrada.readLine()) != null) {
-						dAux = Double.parseDouble(sRespuesta);	//%m
-					}
-					else {	// El dato no se encuentra en la primera línea
-						dAux = -2;	//%m
-					}
+//					double dAux;	//%m
+//					if ((sRespuesta = bfrArchivoEntrada.readLine()) != null) {
+//						dAux = Double.parseDouble(sRespuesta);	//%m
+//					}
+//					else {	// El dato no se encuentra en la primera línea
+//						dAux = -2;	//%m
+//					}
 					
-					boolean bNumNegativos = ( dAux < 0 );	//%a
+//					boolean bNumNegativos = ( dAux < 0 );	//%a
+					boolean bNumNegativos = false;	//%a
 					
 					if (!bNumNegativos) {	//%m// Revisar que el Xk sea válido
-						arcEntrada.dXk = dAux;	//%m
+						arcEntrada.dXk = 1;	//%m		///////////MODIFICAR
 					
 						String sEntrada;	// Guarda la linea completa del archivo
 						int iContLinea = 0;	// Cuenta la línea de datos que se está leyendo
 						
 						while ( (sEntrada = bfrArchivoEntrada.readLine()) != null ) {
-							StringTokenizer tokEntrada = new StringTokenizer(sEntrada, ",");
+							
 							iContLinea++;
 							
-							if (tokEntrada.countTokens() == 2) {
-								double dX = Double.parseDouble(tokEntrada.nextToken());	//%m
-								double dY = Double.parseDouble(tokEntrada.nextToken());	//%m
-								
-								if (dX < 0 || dY < 0) {	//%a// "X" y "Y" deben ser mayores o iguales a 0
-									bNumNegativos = true;	//%a
-									break;	//%a
-								}
-								
-								arcEntrada.AgregarPunto(dX, dY);	//%m
+							String sError = arcEntrada.AgregarInstruccion(sEntrada, iContLinea);
+							
+							if ( sError.equals("") ) {
+								// Trabajar con string
 							}
 							else {
-								System.out.println("No se ha podido agregar la línea " + iContLinea + ". Revise su formato.");
+								System.out.print(sError);
 							}
 						}
 						bfrArchivoEntrada.close();
+						
+						// Procesar instrucciones						
+						if (arcEntrada.vecInstrucciones.size() > 0) {	// Conseguir la primera instrucción
+							int iContInstruccion = 0;
+							Archivo.Instruccion insActual;
+							
+							insActual = arcEntrada.vecInstrucciones.get(0);
+						
+							// Recorrer todas las instrucciones disponibles o hasta que se indique se finaliza el programa
+							while (iContInstruccion < arcEntrada.vecInstrucciones.size()
+									&& !(insActual.sTipo.equals("E")) 
+									) {
+								ManejadorDeMemoria mdmSistemaOperativo = new mdmSistemaOperativo( 2048, 4096, 8 );
+								
+								
+								// Continuar hasta que se indique fin de sección de instrucciones
+								while ( !(insActual.sTipo.equals("F")) ) {
+									if ( insActual.sTipo.equals("P") ) {	// Cargar proceso
+										if ( mdmSistemaOperativo.cargarProceso( insActual.iValor1, insActual.iValor2 ) ) {	// Cargar y manejar errores de carga
+											// SALIDA ESPERADA
+										}
+										else {	// Error. No se pudo cargar el proceso
+											System.out.println("No fue posible cargar el proceso. Posiblemente ya estaba cargado a memoria.");
+										}
+									}
+									else if ( insActual.sTipo.equals("A") ) {	// Accesar a proceso para leer o modificar
+										if ( mdmSistemaOperativo.accesarProceso(insActual.iValor1, insActual.iValor2, insActual.iValor3) ) {	// Accesar y manejar errores
+											// SALIDA ESPERADA
+										}
+										else {	// Error. No se pudo accesar al proceso
+											System.out.println("No fue posible accesar al proceso. Tal vez haya faltado cargarlo a memoria.");
+										}
+									}
+									//POR DESARROLLAR**************************
+									else if ( insActual.sTipo.equals("L") ) {	// Liberar proceso de memoria
+										
+									}
+								}
+							}
+						}
+						else {	// Error. No hay instrucciones
+							System.out.println("El archivo provisto no contiene instrucciones.");
+						}
 						
 						if (!bNumNegativos) {	//%a// Evitar datos menores a 0
 							if (arcEntrada.vecX.size() > 2) {	// Aceptar solo más de 2 parejas de datos
@@ -100,30 +138,23 @@ public class Main {
 							}
 						}
 						else {	//%a// Alguno de las parejas de datos contiene un número negativo
-							System.out.println("El valor de todos los datos debe ser mayor o igual a 0.\n");	//%a
+							System.out.println("El valor de todos los datos debe ser mayor o igual a 0.\n");	
 						}
 					}
 					else {	// El dato Xk es menor a 0
-						System.out.println("El valor de todos los datos debe ser mayor o igual a 0.\n");	//%m
+						System.out.println("El valor de todos los datos debe ser mayor o igual a 0.\n");	
 					}
 					
 				} catch (FileNotFoundException e) { // Manejar errores al abrir el archivo
 					System.out.println("No se ha podido encontrar el archivo indicado\n");
-				} catch (NumberFormatException e) { // Manejar errores al leer datos
-					System.out.println("Alguno de los datos no tiene el formato correcto. "
-							+ "Asegúrese que todos los números sean decimales o enteros, "
-							+ "que la primera línea tenga 1 solo número y que el resto "
-							+ "esté conformado por parejas separadas únicamente una coma.\n");	//%m
-					//%d=3
-				} catch (IOException e) {	// Manejar errores al leer Xk
-					//e.printStackTrace();
-					System.out.println("No se ha podido registrar el dato Xk correctamente.\n");
-				} finally {	//%a
-					if (bfrArchivoEntrada != null) {	//%a
-						try {	//%a
-							bfrArchivoEntrada.close();	//%a
-						} catch (IOException e) {	//%a
-							System.out.println("Ocurrió un error inesperado al intentar cerrar el documento.");	//%a
+				} catch (IOException e) {	// Falta manejar
+					System.out.println("Error inesperado IOException.\n");
+				} finally {
+					if (bfrArchivoEntrada != null) {
+						try {	
+							bfrArchivoEntrada.close();	
+						} catch (IOException e) {	
+							System.out.println("Ocurrió un error inesperado al intentar cerrar el documento.");	
 						}
 					}
 				}
